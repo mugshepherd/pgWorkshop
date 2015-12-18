fa65cc053f22d9228d8fa86498781f83e2d693ce
toby <toby>
2015-12-18 14:08:46 +0300

    Module 13 - contacts.  not working as of this point.

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,6 +1,78 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/cordova-plugin-contacts/www/contacts.js",
+        "id": "cordova-plugin-contacts.contacts",
+        "pluginId": "cordova-plugin-contacts",
+        "clobbers": [
+            "navigator.contacts"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-contacts/www/Contact.js",
+        "id": "cordova-plugin-contacts.Contact",
+        "pluginId": "cordova-plugin-contacts",
+        "clobbers": [
+            "Contact"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-contacts/www/ContactAddress.js",
+        "id": "cordova-plugin-contacts.ContactAddress",
+        "pluginId": "cordova-plugin-contacts",
+        "clobbers": [
+            "ContactAddress"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-contacts/www/ContactError.js",
+        "id": "cordova-plugin-contacts.ContactError",
+        "pluginId": "cordova-plugin-contacts",
+        "clobbers": [
+            "ContactError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-contacts/www/ContactField.js",
+        "id": "cordova-plugin-contacts.ContactField",
+        "pluginId": "cordova-plugin-contacts",
+        "clobbers": [
+            "ContactField"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-contacts/www/ContactFindOptions.js",
+        "id": "cordova-plugin-contacts.ContactFindOptions",
+        "pluginId": "cordova-plugin-contacts",
+        "clobbers": [
+            "ContactFindOptions"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-contacts/www/ContactName.js",
+        "id": "cordova-plugin-contacts.ContactName",
+        "pluginId": "cordova-plugin-contacts",
+        "clobbers": [
+            "ContactName"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-contacts/www/ContactOrganization.js",
+        "id": "cordova-plugin-contacts.ContactOrganization",
+        "pluginId": "cordova-plugin-contacts",
+        "clobbers": [
+            "ContactOrganization"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-contacts/www/ContactFieldType.js",
+        "id": "cordova-plugin-contacts.ContactFieldType",
+        "pluginId": "cordova-plugin-contacts",
+        "merges": [
+            ""
+        ]
+    },
+    {
         "file": "plugins/cordova-plugin-device/www/device.js",
         "id": "cordova-plugin-device.device",
         "pluginId": "cordova-plugin-device",
@@ -43,6 +115,7 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "cordova-plugin-console": "1.0.2",
+    "cordova-plugin-contacts": "1.1.0",
     "cordova-plugin-device": "1.1.0",
     "cordova-plugin-dialogs": "1.2.0",
     "cordova-plugin-geolocation": "1.0.1",
--- www/js/EmployeeView.js
+++ www/js/EmployeeView.js
@@ -3,14 +3,13 @@ var EmployeeView = function(employee) {
     this.initialize = function() {
         this.$el = $('<div/>');
         this.$el.on('click', '.add-location-btn', this.addLocation);
+        this.$el.on('click', '.add-contact-btn', this.addToContacts);
     };
 
     this.render = function() {
         this.$el.html(this.template(employee));
         return this;
     };
-    
-
     
     this.addLocation = function(event) {
         event.preventDefault();
@@ -25,6 +24,23 @@ var EmployeeView = function(employee) {
             {maximumAge: 3000, timeout: 5000, enableHighAccuracy: true}
         );
         return false;
+    };
+    
+    this.addToContacts = function(event) {
+        event.preventDefault();
+        console.log('addToContacts');
+        if (!navigator.contacts) {
+            alert("Contacts API not supported", "Error");
+            return;
+        }
+        var contact = navigator.contacts.create();
+        contact.name = {givenName: employee.firstName, familyName: employee.lastName};
+        var phoneNumbers = [];
+         phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+         phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, truee);
+         contact.phoneNumbers = phoneNumbers;
+         contact.save();
+         return false;
     };
 
     this.initialize();
